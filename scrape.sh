fcec72f851349a308d1ff5b2de4673d6ae6bcb4f
stud1190 <130294447+stud1190@users.noreply.github.com>
2023-05-11 03:01:58 +0000

    Use HTTrack

--- scrape.sh
+++ scrape.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-wget --mirror --convert-links -E $@ "https://1337x.to"
+## wget --mirror --convert-links -E $@ "https://1337x.to"
+httrack -O 1337x 'https://1337x.to' --sockets=8 -p1 --continue --cache=1
