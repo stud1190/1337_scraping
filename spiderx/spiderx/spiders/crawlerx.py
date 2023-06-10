b861892de4002cae95b3da57033cd462f2489ae2
stud1190 <130294447+stud1190@users.noreply.github.com>
2023-06-10 02:43:59 +0000

    improve recorded data format

--- spiderx/spiderx/spiders/crawlerx.py
+++ spiderx/spiderx/spiders/crawlerx.py
@@ -1,6 +1,17 @@
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
 
+import unicodedata
+
+def parse_int(text: str):
+    '''
+    Returns integer by parsing the text, or None if it cannot be parsed. 
+    '''
+    try:
+        return int(text)
+    except ValueError:
+        return None
 
 class CrawlerxSpider(CrawlSpider):
     '''
@@ -35,9 +46,19 @@ class CrawlerxSpider(CrawlSpider):
 
     def parse_torrent(self, response):
         scr_urls, th_urls = self.image_urls(response)
+        h1 = response.xpath('//h1/text()').get()
+        if not str(h1).strip():
+            return
+        title = response.css('title::text').get().lstrip('Download ').rstrip('| 1337x')
+        desc_node = response.css('#description').get()
+        description = None
+        if (desc_node is not None):
+            plaintext = BeautifulSoup(desc_node, 'html.parser').get_text()
+            description = unicodedata.normalize('NFKD', plaintext)
+
         yield {
             'page_url': response.url,
-            'title': response.xpath('//h1/text()').get(),
+            'title': title,
             'category': self.Q(response, "Category"),
             'type': self.Q(response, "Type"),
             'language': self.Q(response, "Language"),
@@ -46,10 +67,10 @@ class CrawlerxSpider(CrawlSpider):
             'downloads': self.Q(response, "Downloads"),
             'last_checked': self.Q(response, "Last checked"),
             'date_uploaded': self.Q(response, "Date uploaded"),
-            'seeders': self.Q(response, 'Seeders'),
-            'leechers': self.Q(response, "Leechers"),
+            'seeders': parse_int(self.Q(response, 'Seeders')),
+            'leechers': parse_int(self.Q(response, "Leechers")),
             'magnet': self.extract_magnet(response),
-            'screenshot_source_urls': scr_urls,
+            'screenshot_urls': scr_urls,
             'screenshot_thumb_urls': th_urls,
-            ## 'description': TODO
+            'description': description,
         }
